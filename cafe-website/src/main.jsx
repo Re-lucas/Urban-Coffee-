d4f2fb0899b2f86ea5f4e33e241ce38d165fb7fc
Re-lucas <wuw81013@gmail.com>
2025-06-02 12:45:07 -0400

    “我的账户” 页面能够正常渲染

--- cafe-website/src/main.jsx
+++ cafe-website/src/main.jsx
@@ -1,14 +1,20 @@
 // src/main.jsx
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './styles/main.css'
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './styles/main.css';
+import { BrowserRouter } from 'react-router-dom';
+
+// 引入 AuthProvider
+import { AuthProvider } from './context/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      {/* 用 AuthProvider 包裹 App 组件 */}
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
-)
+);
