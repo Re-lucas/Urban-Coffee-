f7b08d30be77189c01a000793cd7f5db4bd7f0e3
Re-lucas <wuw81013@gmail.com>
2025-06-04 21:57:30 -0400

    配置全局 Axios 实例

---
+++ cafe-website/src/utils/axiosConfig.js
@@ -0,0 +1,25 @@
+// src/utils/axiosConfig.js
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL + '/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// 请求拦截器：如果 localStorage 里有 userInfo.token，就自动带上 Authorization 头
+api.interceptors.request.use(
+  (config) => {
+    const userInfo = JSON.parse(localStorage.getItem('userInfo'));
+    if (userInfo && userInfo.token) {
+      config.headers.Authorization = `Bearer ${userInfo.token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default api;
